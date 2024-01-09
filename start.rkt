339d8678011bd81800834dc715d8068e715ec1fe
Douglas Camargo <douglas@thecodesoup.com>
2024-01-08 22:01:54 -0300

    Adding BSL introduction problems

---
+++ start.rkt
@@ -0,0 +1,15 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname start) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require htdp/image)
+
+(define RADIUS 100)
+
+(define (create_circle c r)
+  (circle r "solid" c)
+  )
+
+(overlay (create_circle "blue" RADIUS)
+         (create_circle "green" (* RADIUS 0.75))
+         (create_circle "red" (* RADIUS 0.5))
+         (create_circle "yellow" (* RADIUS 0.25)))
